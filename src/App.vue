75645be46698669d6cdc05f21c3a9c0ff1a3e216
Galen Lockwood <galenlockwood@gmail.com>
2021-08-11 15:13:59 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <main>
+    <user-list :users="activeUsers" @list-projects="selectUser"></user-list>
+    <projects-list :user="selectedUser"></projects-list>
+  </main>
+</template>
+
+<script>
+    import USER_DATA from './dummy-data.js';
+    import UserList from './components/users/UserList.vue';
+    import ProjectsList from './components/projects/ProjectsList.vue';
+
+    import {
+        ref
+    } from 'vue';
+
+    export default {
+        components: {
+            UserList,
+            ProjectsList,
+        },
+        setup() {
+            const selectedUser = ref(null);
+            // No need to store USER_DATA in a ref since it is data that never changes
+            const activeUsers = USER_DATA;
+
+            // Note: we padd in the iser ID (uid) as an argument to sort through
+            // "value" in this case refers to the uid which is the argument we pass in
+            // "usr" stands for the user/users we are filtering through    
+            function selectUser(uid) {
+                selectedUser.value = activeUsers.find((usr) => usr.id === uid);
+            }
+
+            return {
+                selectedUser,
+                activeUsers,
+                selectUser
+            }
+        },
+        ///////////////////// Data in the Options API format /////////////////////////
+        // data() {
+        //     return {
+        //         selectedUser: null,
+        //         activeUsers: USER_DATA,
+        //     };
+        // },
+        ///////////////////// Methods in the Options API format /////////////////////////
+        // methods: {
+        //     selectUser(uid) {
+        //         this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
+        //     },
+        // },
+    };
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    
+    html {
+        font-family: sans-serif;
+    }
+    
+    body {
+        margin: 0;
+    }
+    
+    main {
+        display: flex;
+        justify-content: space-around;
+    }
+    
+    button {
+        font: inherit;
+        border: 1px solid #00006b;
+        background-color: transparent;
+        color: #00006b;
+        padding: 0.5rem 1.5rem;
+        cursor: pointer;
+        margin: 0.5rem 0.5rem 0.5rem 0;
+    }
+    
+    button:hover,
+    button:active {
+        background-color: #efefff;
+    }
+    
+    button.selected {
+        background-color: #00006b;
+        color: white;
+    }
+</style>
---
+++ src/components/users/UserList.vue
@@ -0,0 +1,173 @@
+<template>
+  <base-container>
+    <h2>Active Users</h2>
+    <base-search @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <div>
+      <button @click="sort('asc')" :class="{selected: sorting === 'asc'}">Sort Ascending</button>
+      <button @click="sort('desc')" :class="{selected: sorting === 'desc'}">Sort Descending</button>
+    </div>
+    <ul>
+      <user-item
+        v-for="user in displayedUsers"
+        :key="user.id"
+        :user-name="user.fullName"
+        :id="user.id"
+        @list-projects="$emit('list-projects', $event)"
+      ></user-item>
+    </ul>
+  </base-container>
+</template>
+
+<script>
+    import UserItem from './UserItem.vue';
+    import {
+        ref,
+        computed,
+        watch
+    } from 'vue';
+    export default {
+        components: {
+            UserItem,
+        },
+        // users gets provided via props for the computed function
+        props: ['users'],
+        emits: ['list-projects'],
+        // We want to pass props to the setup() function as an argument
+        // In this case props will have a user key 
+        setup(props) {
+            /////////////// Search related ///////////////
+            const enteredSearchTerm = ref('');
+            // Search term is empty to store user input
+            const activeSearchTerm = ref('');
+
+            const availableUsers = computed(function() {
+                let users = [];
+                if (activeSearchTerm.value) {
+                    // props instead of value is used because props is being returned 
+                    // We pass in props before any place we reference users
+                    users = props.users.filter((usr) =>
+                        usr.fullName.includes(activeSearchTerm.value)
+                    );
+                } else if (props.users) {
+                    users = props.users;
+                }
+                return users;
+            });
+
+            // For the watch we pass in the dependency it should watch for
+            // In this case it is the "enteredSearchTerm"
+            watch(enteredSearchTerm, function(newValue) {
+                setTimeout(() => {
+                    if (newValue === enteredSearchTerm.value) {
+                        activeSearchTerm.value = newValue;
+                    }
+                }, 300);
+            });
+
+            // At the end of our search logic we return the updated search value 
+            // val is the value chosen by the user
+            function updateSearch(val) {
+                enteredSearchTerm.value = val;
+            }
+
+
+            /////////////// Sorting related ///////////////
+            const sorting = ref(null);
+
+            const displayedUsers = computed(function() {
+                if (!sorting.value) {
+                    // we use .value here because computed properties are stored as read only refs (as in the ref above)
+                    return availableUsers.value;
+                }
+                // Slice is used to return a new sorted array so we don't sort the existing one
+                return availableUsers.value.slice().sort((u1, u2) => {
+                    if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+                        return 1;
+                    } else if (sorting.value === 'asc') {
+                        return -1;
+                    } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+            });
+
+            function sort(mode) {
+                sorting.value = mode;
+            }
+
+            return {
+                enteredSearchTerm,
+                updateSearch,
+                displayedUsers,
+                sorting,
+                sort
+            };
+        },
+        /////////////////////// Original Data 
+        // data() {
+        //     return {
+        //         enteredSearchTerm: '',
+        //         activeSearchTerm: '',
+        //         sorting: null,
+        //     };
+        // },
+        /////////////////////// Original Computed
+        // computed: {
+        //     availableUsers() {
+        //         let users = [];
+        //         if (this.activeSearchTerm) {
+        //             users = this.users.filter((usr) =>
+        //                 usr.fullName.includes(this.activeSearchTerm)
+        //             );
+        //         } else if (this.users) {
+        //             users = this.users;
+        //         }
+        //         return users;
+        //     },
+        //     displayedUsers() {
+        //         if (!this.sorting) {
+        //             return this.availableUsers;
+        //         }
+        //         return this.availableUsers.slice().sort((u1, u2) => {
+        //             if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
+        //                 return 1;
+        //             } else if (this.sorting === 'asc') {
+        //                 return -1;
+        //             } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
+        //                 return -1;
+        //             } else {
+        //                 return 1;
+        //             }
+        //         });
+        //     },
+        // },
+        //////////////////////// Original methods
+        // methods: {
+        //     updateSearch(val) {
+        //         this.enteredSearchTerm = val;
+        //     },
+        //     sort(mode) {
+        //         this.sorting = mode;
+        //     },
+        // },
+        // watch: {
+        //     enteredSearchTerm(val) {
+        //         setTimeout(() => {
+        //             if (val === this.enteredSearchTerm) {
+        //                 this.activeSearchTerm = val;
+        //             }
+        //         }, 300);
+        //     }
+        // },
+    };
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
